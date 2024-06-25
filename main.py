c5a091b8c6d3561cb7005b47bbda1c448d2fd771
knoxthinks <itsknox@gmail.com>
2024-06-25 15:34:25 -0400

    function counter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+# my solution
 def main():
     with open('books/frankenstein.txt') as f:
 
@@ -10,10 +10,32 @@ if __name__ == "__main__":
 
 def count():
     words = len(main().split())
-    print(words)
+    print(f"{words} words found in the document")
 
 count()
 
+def dicts():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    lowercased = text.lower()
+    answer = {}
+    for char in lowercased:
+        if char in answer:
+            answer[char] += 1
+        else:
+            answer[char] = 1
+    print(answer)
+
+        # Print each character and its count on a new line
+    for char, count in answer.items():
+        print(f"'{char}': {count}")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+dicts()
 # This was boot.dev solution
 
 # def main():
